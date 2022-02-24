681b169eb5a865c84f85f2cd52e959183f450e5e
JhonFiUNFV <47376395+JhonFiUNFV@users.noreply.github.com>
2022-02-24 12:04:13 -0500

    #Limpiar y direccionar el set de datos.

--- ModuloI_EstadisticaparaCienciadeDatos/Sesion02_InferenciaEstadisticayPruebasdeHipotesis/LaboratorioAplicacion/lab2.R
+++ ModuloI_EstadisticaparaCienciadeDatos/Sesion02_InferenciaEstadisticayPruebasdeHipotesis/LaboratorioAplicacion/lab2.R
@@ -2,9 +2,10 @@
 ################------- LABORATORIO 02 -------------#########################
 #############################################################################
 
-
 #Limpiar y direccionar el set de datos.
 rm(list = ls())
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 getwd()
-
+data_churn.csv <- read.csv("DataChurn.csv",sep= ";")
+View(loan_prediction.csv)
+str(loan_prediction.csv)
