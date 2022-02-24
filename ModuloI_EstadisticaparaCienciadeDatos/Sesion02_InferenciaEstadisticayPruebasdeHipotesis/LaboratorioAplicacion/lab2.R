5cdb10995b3c7f837ec65cc37f11dc7877df88c1
JhonFiUNFV <47376395+JhonFiUNFV@users.noreply.github.com>
2022-02-24 13:05:58 -0500

    Deteccion de missing

--- ModuloI_EstadisticaparaCienciadeDatos/Sesion02_InferenciaEstadisticayPruebasdeHipotesis/LaboratorioAplicacion/lab2.R
+++ ModuloI_EstadisticaparaCienciadeDatos/Sesion02_InferenciaEstadisticayPruebasdeHipotesis/LaboratorioAplicacion/lab2.R
@@ -2,10 +2,27 @@
 ################------- LABORATORIO 02 -------------#########################
 #############################################################################
 
+library(DataExplorer)
+library(data.table)
+
 #Limpiar y direccionar el set de datos.
 rm(list = ls())
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 getwd()
-data_churn.csv <- read.csv("DataChurn.csv",sep= ";")
-View(loan_prediction.csv)
-str(loan_prediction.csv)
+data <- read.csv("DataChurn.csv",sep= ",")
+View(data)
+str(data)
+churn <-fread("DataChurn.csv",
+                  header=T, 
+                  verbose =FALSE, 
+                  stringsAsFactors=TRUE,
+                  showProgress =TRUE)
+
+str(churn)
+
+
+#Deteccion de missing
+plot_missing(churn) 
+
+
+
