98e39241759d98b37b1ee25d118ca59ae734dd4a
JhonFiUNFV <47376395+JhonFiUNFV@users.noreply.github.com>
2022-02-24 21:36:51 -0500

    cambios

--- ModuloI_EstadisticaparaCienciadeDatos/Sesion02_InferenciaEstadisticayPruebasdeHipotesis/LaboratorioAplicacion/lab2.R
+++ ModuloI_EstadisticaparaCienciadeDatos/Sesion02_InferenciaEstadisticayPruebasdeHipotesis/LaboratorioAplicacion/lab2.R
@@ -4,25 +4,37 @@
 
 library(DataExplorer)
 library(data.table)
+library(scatterplot3d)
+library(moments) 
 
-#Limpiar y direccionar el set de datos.
+# Limpiar y direccionar el set de datos.
 rm(list = ls())
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 getwd()
 data <- read.csv("DataChurn.csv",sep= ",")
-View(data)
 str(data)
-churn <-fread("DataChurn.csv",
+data_churn <-fread("DataChurn.csv",
                   header=T, 
                   verbose =FALSE, 
                   stringsAsFactors=TRUE,
                   showProgress =TRUE)
 
-str(churn)
+str(data_churn)
 
 
-#Deteccion de missing
+# Deteccion de missing
 plot_missing(churn) 
+
+# Gr??fica
+ggplot(data_churn ,aes(CHURN, INGRESO, colour = CHURN ))+geom_point() + geom_boxplot()
+
+# Anova
+anova1 <- aov(INGRESO~CHURN,data = data_churn)
+summary(anova1)
+par(mfrow=c(2,2)) #Particiona mi ventana grafica
+plot(anova1) #Contrasta los supuestos del ANOVA, de forma grafica
+
+agostino.test(data_churn$INGRESO)
 
 
 
